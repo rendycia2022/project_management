b7e39b7c3e7fb276f789e40ae48e7a56c1a8b32b
rendycia2022 <rendy.cia2022@gmail.com>
2024-09-13 13:54:00 +0700

    Update Chart and styling

--- app-backend/app/Http/Controllers/project/ChartController.php
+++ app-backend/app/Http/Controllers/project/ChartController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Hash;
 
 // models
 use App\Models\projectModels\ListModel;
+use App\Models\projectModels\styleModels;
 
 
 class ChartController extends BaseController
@@ -22,6 +23,7 @@ class ChartController extends BaseController
 
         // models
         $this->ListModel = new ListModel;
+        $this->styleModels = new styleModels;
         
 
     }
@@ -35,12 +37,12 @@ class ChartController extends BaseController
         );
         $list = $this->ListModel->getData($where);
 
-        $filtered_data = array();
-
+        // filtering data
+        $raw = array();
         $optionYears = array();
 
         $count_list = count($list);
-        if($count_list>0){
+        if($count_list > 0){
             for($i=0; $i<$count_list; $i++){
                 $date_create = date_create($list[$i]['date']);
                 $date_formated = date_format($date_create,"Y");
@@ -48,24 +50,98 @@ class ChartController extends BaseController
                 $optionYears[$date_formated] = $date_formated;
 
                 if($year == "false"){
-                    $filtered_data[] = $list[$i];
+                    $raw[] = $list[$i];
                 }else{
                     if($year == $date_formated){
-                        $filtered_data[] = $list[$i];
+                        $raw[] = $list[$i];
                     }
                 }
             }
         }
+
+        // data chart builder
+        // define data
+        $chart = array();
+        $count_raw = count($raw);
+        if($count_raw > 0){
+            for($j=0; $j<$count_raw; $j++){
+                $project_code = $raw[$j]['project']['code'];
+
+                $charts[$project_code]['title'] = $project_code;
+
+                // build data
+                $revenue = $raw[$j]['revenue']['total'];
+                $invoice = $raw[$j]['invoice']['total'];
+                $indirect = $raw[$j]['indirect']['total'];
+                $direct = $raw[$j]['direct']['total'];
+                if(isset($charts[$project_code]['raw'])){
+                    // expected revenue
+                    $charts[$project_code]['raw']['revenue'] = $charts[$project_code]['raw']['revenue'] + $revenue;
+
+                    // invoice
+                    $charts[$project_code]['raw']['invoice'] = $charts[$project_code]['raw']['invoice'] + $invoice;
+
+                    // indirect
+                    $charts[$project_code]['raw']['indirect'] = $charts[$project_code]['raw']['indirect'] + $indirect;
+
+                    // direct
+                    $charts[$project_code]['raw']['direct'] = $charts[$project_code]['raw']['direct'] + $direct;
+                }else{
+                    // expected revenue
+                    $charts[$project_code]['raw']['revenue'] = $revenue;
+
+                    // invoice
+                    $charts[$project_code]['raw']['invoice'] = $invoice;
+
+                    // indirect
+                    $charts[$project_code]['raw']['indirect'] = $indirect;
+
+                    // direct
+                    $charts[$project_code]['raw']['direct'] = $direct;
+                }
+            }
+        }
+
+        // build chart
+        $charts = array_values($charts);
+
+        for($c=0; $c<count($charts); $c++){
+            $dataset =  $charts[$c]['raw'];
+
+            $balance = $dataset['revenue'] - $dataset['invoice'];
+            $charts[$c]['dataset'] = [$dataset['invoice'], $balance];
+
+            // legend
+            $charts[$c]['labels'] = ["Invoice", "Expected Revenue's left"];
+            
+            $progress = round(($balance / $dataset['revenue']) * 100);
+            if($dataset['invoice'] >= $dataset['revenue']){
+                $progress = 100;
+            }
+
+            $charts[$c]['progress'] = $progress;
+
+            // generating color
+            $colors = $this->styleModels->project_color($charts[$c]['title']);
+
+            $charts[$c]['color'] = $colors;
+            $charts[$c]['datasetColor'] = ['#cccccc', $colors];
+        }
+
+        $key_values = array_column($charts, 'title');
+        array_multisort($key_values, SORT_DESC, $charts);
         
         // reindexing
         $optionYears = array_values($optionYears);
+        rsort($optionYears);
 
         $response = array(
             "status"=>200,
             "message"=>"Ok.",
-            "filtered_data"=>$filtered_data,
+            "raw"=>$raw,
             "year"=>$year,
             "optionYears"=>$optionYears,
+            "charts"=>$charts,
         );
 
         return response()->json($response);
--- app-backend/app/Models/projectModels/styleModels.php
+++ app-backend/app/Models/projectModels/styleModels.php
@@ -5,8 +5,18 @@ namespace App\Models\projectModels;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
+// models
+use App\Models\projectModels\ListModel;
+
 class styleModels extends Model
 {
+    public function __construct(){
+
+        // models
+        $this->ListModel = new ListModel;
+        
+
+    }
 
     public function colors(){
 
@@ -22,5 +32,14 @@ class styleModels extends Model
         );
         
         return $data;
+    }
+
+    public function project_color($code){
+
+        $data['SLB_XL'] = '#febe98';
+        $data['SLB_IOH'] = '#b38a6a';
+        $data['IOH'] = '#f97272';
+        
+        return $data[$code];
     }
 }
